84a90da05d3c3ea96661f47c7d487273cd8f44d8
eagle9111 <alhassan.khalilnew@gmail.com>
2025-12-14 15:25:06 +0200

    final

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,31 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     host: "0.0.0.0",
-
     allowedHosts: [
       "melisa-balsamiferous-aubrie.ngrok-free.dev",
-      ".ngrok-free.dev" 
+      ".ngrok-free.dev",
     ],
   },
-})
+
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+});
