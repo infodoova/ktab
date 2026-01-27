26d819455adf9bd445566d763a3ca831c3b72860
eagle9111 <alhassan.khalilnew@gmail.com>
2026-01-27 14:38:30 +0200

    gfgf

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.t8dt58mnpi"
+    "revision": "0.8ot2ng2mu18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
